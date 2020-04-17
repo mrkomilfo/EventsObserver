a7df406ea11405530d6106de94555bdbaa7311e9
mrkomilfo <43483871+mrkomilfo@users.noreply.github.com>
2020-04-17 05:49:00 +0300

    add exception classes

---
+++ src/TrainingProject/TrainingProject.Domain.Logic/Exceptions/EventException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TrainingProject.DomainLogic.Exceptions
+{
+    public class EventException : Exception
+    {
+        public EventException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ src/TrainingProject/TrainingProject.Domain.Logic/Exceptions/UserException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TrainingProject.DomainLogic.Exceptions
+{
+    public class UserException : Exception
+    {
+        public UserException(string message) : base(message)
+        {
+        }
+    }
+}
